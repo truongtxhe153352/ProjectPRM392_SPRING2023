e9320ff80dc7c43a10e7a13a081018ddc99a2366
truongtxhe153352 <truongtxhe153352@fpt.edu.vn>
2023-03-13 22:18:36 +0700

    Push selection of after scan

--- app/src/main/java/com/example/projectprm392_spring2023/MainActivity.java
+++ app/src/main/java/com/example/projectprm392_spring2023/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.projectprm392_spring2023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editContent;
+    private Button btnSend;
+
+    public static final String CONTENT = "CONTENT";
+
+
+    public MainActivity() {
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editContent = (EditText) findViewById(R.id.edt_content);
+        btnSend = (Button) findViewById(R.id.btn_sendData);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content = editContent.getText().toString();
+                byExtras(content);
+            }
+        });
+    }
+
+    public void byExtras(String content) {
+        Intent intent = new Intent(MainActivity.this, ShowAfterScan.class);
+        intent.putExtra(CONTENT, content);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/projectprm392_spring2023/ShowAfterScan.java
@@ -0,0 +1,69 @@
+package com.example.projectprm392_spring2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ShowAfterScan extends AppCompatActivity {
+
+    String[] items = {"Summary", "Give the answer", "Rewrite the passage", "Custom"};
+
+    AutoCompleteTextView autoCompleteTxt;
+
+    ArrayAdapter<String> arrayAdapterItems;
+
+    private TextView tvContent;
+
+    private EditText edtCustom;
+
+    public ShowAfterScan() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_after_scan);
+
+        tvContent = (TextView) findViewById(R.id.tv_content);
+
+        // Get data form textView to editText
+        edtCustom = (EditText)findViewById(R.id.edt_customContent);
+
+        setDataByExtra();
+
+        // show select item
+        autoCompleteTxt = findViewById(R.id.auto_complete_txt);
+
+        arrayAdapterItems = new ArrayAdapter<String>(this, R.layout.list_item, items);
+
+        autoCompleteTxt.setAdapter(arrayAdapterItems);
+
+        autoCompleteTxt.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(), "Item: " + item, Toast.LENGTH_SHORT).show();
+
+                // Display content to text View custom
+                if (item.equals("Custom")){
+                    edtCustom.setText(tvContent.getText().toString());
+                }
+            }
+        });
+    }
+
+    public void setDataByExtra() {
+        Intent intent = getIntent();
+        String content = intent.getStringExtra(MainActivity.CONTENT);
+
+        tvContent.setText(content);
+    }
+}
