cb84357a32adb07c3867b75ef3b4dfa621bb57dd
thaiduongme <thaiduong.personal@gmail.com>
2023-03-15 11:42:36 +0700

    add chatgpt response/request

---
+++ app/src/main/java/com/example/projectprm392_spring2023/ChatGptResponse.java
@@ -0,0 +1,17 @@
+package com.example.projectprm392_spring2023;
+
+public class ChatGptResponse {
+    private String response;
+
+    public ChatGptResponse(String response) {
+        this.response = response;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+}
